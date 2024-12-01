7bdeb7864ba3dec4f61d5ebfc48bdcf0edfebecb
Tyler Beckman <ty@myriation.xyz>
2024-12-01 14:10:32 -0700

    reformat and clean up

--- day1/part1.ts
+++ day1/part1.ts
@@ -1,16 +1,18 @@
-const input = await Deno.readTextFile("./input.txt").then(
-    (f) =>
-        f.split("\n").reduce<[number[], number[]]>(
-            (
-                acc,
-                cur,
-            ) => [[...acc[0], +cur.split("   ")[0]], [
-                ...acc[1],
-                +cur.split("   ")[1],
-            ]],
-            [[], []],
-        ),
-).then(([first, second]) => [first.toSorted(), second.toSorted()]).then((
-    [first, second],
-) => first.reduce((acc, cur, i) => acc + Math.abs(cur - second[i]), 0));
-console.log("Answer = " + input);
+export async function solve() {
+    return await Deno.readTextFile("./input.txt").then(
+        (f) =>
+            f.split("\n").reduce<[number[], number[]]>(
+                (
+                    acc,
+                    cur,
+                ) => [
+                    [...acc[0], +cur.split("   ")[0]],
+                    [...acc[1], +cur.split("   ")[1]],
+                ],
+                [[], []],
+            ),
+    ).then(([first, second]) => [first.toSorted(), second.toSorted()]).then(
+        ([first, second]) =>
+            first.reduce((acc, cur, i) => acc + Math.abs(cur - second[i]), 0),
+    );
+}
--- day1/part2.ts
+++ day1/part2.ts
@@ -1,24 +1,25 @@
-const input = await Deno.readTextFile("./input.txt").then(
-    (f) =>
-        f.split("\n").reduce<[number[], number[]]>(
-            (
-                acc,
-                cur,
-            ) => [[...acc[0], +cur.split("   ")[0]], [
-                ...acc[1],
-                +cur.split("   ")[1],
-            ]],
-            [[], []],
-        ),
-).then(([first, second]) => [
-    first,
-    second.reduce((acc, cur) => {
-        if (acc[cur] !== undefined) acc[cur]++;
-        else acc[cur] = 1;
+export async function solve() {
+    return await Deno.readTextFile("./input.txt").then(
+        (f) =>
+            f.split("\n").reduce<[number[], number[]]>(
+                (
+                    acc,
+                    cur,
+                ) => [
+                    [...acc[0], +cur.split("   ")[0]],
+                    [...acc[1], +cur.split("   ")[1]],
+                ],
+                [[], []],
+            ),
+    ).then(([first, second]) => [
+        first,
+        second.reduce((acc, cur) => {
+            if (acc[cur] !== undefined) acc[cur]++;
+            else acc[cur] = 1;
 
-        return acc;
-    }, {} as Record<number, number>),
-]).then(([first, map]) =>
-    first.reduce((acc, cur) => acc + ((map[cur] ?? 0) * cur), 0)
-);
-console.log("Answer = " + input);
+            return acc;
+        }, {} as Record<number, number>),
+    ] as const).then(([first, map]) =>
+        first.reduce((acc, cur) => acc + ((map[cur] ?? 0) * cur), 0)
+    );
+}
