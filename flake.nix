492bf51c0dab3a590efec0d4adf9f87ab1d6dfbe
Tyler Beckman <ty@myriation.xyz>
2024-12-02 21:55:59 -0700

    Add nix flake shell stuff i think

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+    description = "Ty's advent of code 2024 solutions";
+
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    inputs.flake-utils.url = "github:numtide/flake-utils";
+
+    outputs = { nixpkgs, flake-utils, ... }:
+        flake-utils.lib.eachDefaultSystem (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
+            devShells.default = pkgs.mkShellNoCC {
+                packages = with pkgs; [
+                    deno
+                    kotlin
+                ];
+            };
+        });
+}
